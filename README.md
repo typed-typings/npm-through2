66728e19e52fe853130d788c1878a7049a6d7bdf
Homa Wong <homawong@gmail.com>
2016-03-07 00:12:30 -0800

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Typed Through 2
+The type definition for [`through2`](https://github.com/rvagg/through2).
+
+# LICENSE
+MIT
---
+++ test/test.ts
@@ -0,0 +1,5 @@
+/// <reference path="../bundle.d.ts" />
+
+import * as through2 from 'through2';
+
+through2.obj(function() { }, (function() { }));
